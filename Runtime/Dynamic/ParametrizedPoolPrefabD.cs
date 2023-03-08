15c1848aa52d357b1cec4b987d620f420e475f55
Alex Strilchuk <alex.strilchuk@reality.co>
2023-03-08 09:02:28 +0000

    implement the CustomScrolllRect pooling list that supports the multiple types pooling

---
+++ Runtime/Dynamic/ParametrizedPoolPrefabD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace ObjectPool.Dynamic
+{
+    /// <summary>
+    /// Script that must be attatched to prefab for spawning in scroll list
+    /// </summary>
+    [Serializable]
+    public abstract class ParametrizedPoolPrefabD<TData, TEnum> : PoolPrefabD, IParametrizedPoolDataD<TEnum>
+        where TData: IParametrizedPoolDataD<TEnum> 
+        where TEnum: Enum
+    {
+        public abstract TEnum PoolElementType { get; }
+
+        public abstract void HandleDataOnSetup(TData data);
+
+        public virtual void Setup(TData data)
+        {
+            Setup(data.PoolElementId, data.PrefabHeight, data.PrefabVerticalPosition);
+            HandleDataOnSetup(data);
+        }
+    }
+}
